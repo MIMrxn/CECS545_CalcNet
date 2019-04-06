a8449ebb6550033aa389243bc91a286d53289be4
MIMrxn <Fractal@Marvins-MacBook-Pro.local>
2019-04-06 05:08:26 -0700

    implemented canvas display

--- assets/scripts.js
+++ assets/scripts.js
@@ -69,9 +69,12 @@ class MachineI {
 	}
 
 	run() {
+		//var keep_drawing = true;
 		var curr_stmt = null;
 		for(var i=0; i<this.inputs.length-1; i++) {
 			curr_stmt = this.inputs[i].split(".")[0];
+			//window.setTimeout(this.draw(curr_stmt), 1000);
+			//this.draw(curr_stmt);
 			console.log("This is the current statement: " + curr_stmt + "\n");
 			
 			var curr_result = null;
@@ -84,6 +87,8 @@ class MachineI {
 				return false;
 			} else {
 				this.results.push(curr_result);
+				//window.requestAnimationFrame(this.draw());
+				this.draw(curr_stmt);
 			}
 		}
 
@@ -93,6 +98,62 @@ class MachineI {
 			alert("There was an error retrieving all the results.");
 			return false;
 		}
+	}
+
+	draw(curr_stmt) {
+		var machines_canvas = document.getElementById("machines_canvas");
+		var ctx = machines_canvas.getContext("2d");
+		
+		//	Inputs rectangle
+		ctx.font = "14px Arial";
+		ctx.fillText("Input Expression", 0+25, 0+25-4);
+		ctx.clearRect(0+25, 0+25, 200, 132);
+		ctx.rect(0+25, 0+25, 200, 132);
+		ctx.stroke();
+		//	Results rectangle
+		ctx.fillText("Result", 0+25+200, 0+25-4);
+		ctx.clearRect(0+25+200, 0+25, 50, 132);
+		ctx.rect(0+25+200, 0+25, 50, 132);
+		ctx.stroke();
+		
+		ctx.fillText("Machine I", (0+25+200)/2, 0+25+132+14);
+
+		//	Populate inputs
+		for(var i=0; i<this.inputs.length-1; i++) {
+			var input_text = this.inputs[i].split(".")[0];
+			if(input_text===curr_stmt) {
+				ctx.clearRect(0+25+8, 0+25+5+(i*25), 185, 23);
+				ctx.rect(0+25+8, 0+25+5+(i*25), 185, 23);
+				ctx.stroke();
+			}
+			if(ctx.measureText(input_text).width > 140) {
+				console.log("Width of "+i+": "+ctx.measureText(input_text).width);
+				input_text = input_text.substring(0, 22);
+				input_text += "...";
+			}
+			ctx.fillText(input_text, 0+25+12, 0+25+22+(i*25));
+		}
+
+		//	Populate results
+		for(var i=0; i<this.results.length; i++) {
+			var result = this.results[i];
+			ctx.clearRect(0+25+8-3+200, 0+25+5+(i*25), 40, 23);
+			ctx.rect(0+25+8-3+200, 0+25+5+(i*25), 40, 23);
+			ctx.stroke();
+			ctx.fillText(result, 0+25+8+200, 0+25+22+(i*25));
+		}
+
+		/*
+		if(this.results.length != 5) {
+			console.log("This is the results length in draw: "+this.results.length+"\n");
+			window.requestAnimationFrame(this.draw());
+		}
+		*/
+	}
+
+	//	For animation use
+	update() {
+
 	}
 }
 
@@ -105,13 +166,61 @@ class MachineD {
 		if(this.vars_array[letter] === null) {
 			alert("There is no value associated with that value.");
 		} else {
-			return this.vars_array[letter];
+			var loaded_value = this.vars_array[letter];
+			this.draw("Load", letter, loaded_value);
+			return loaded_value;
 		}
 	}
 
 	store(letter, const_value) {
 		this.vars_array[letter] = const_value;
+		this.draw("Store", letter, const_value);
 		return true;
+	}
+
+	draw(msg_type, letter, const_value) {
+		var machines_canvas = document.getElementById("machines_canvas");
+		var ctx = machines_canvas.getContext("2d");
+		
+		//	Draw connection line
+		//	X = origin + initial offset + machine I input box width + machine I result width + connection + quarter of machine A width
+		//	Y = origin + initial offset + machine I input/result box height / 2 + machine A height box / 2
+		ctx.moveTo(0+25+(200+50)+100+(185+50)/4, 0+25+(132/2)+(30/2));
+		ctx.lineTo(0+25+(200+50)+100+(185+50)/4, 0+25+(132/2)+(30/2)+100);
+		ctx.stroke();
+
+		//	Message Type Rectangle
+		ctx.clearRect(0+25+(200+50)+100, 0+25+132/2+(30/2)+100, 75, 30);
+		ctx.rect(0+25+(200+50)+100, 0+25+(132/2)+(30/2)+100, 75, 30);
+		ctx.stroke();
+		ctx.font = "14px Arial";
+		ctx.fillText(msg_type, 0+25+(200+50)+100+20, 0+25+(132/2)+(30/2)+100+20);
+		ctx.fillText("Type", 0+25+(200+50)+100, 0+25+(132/2)+(30/2)+100-4);
+			
+		//	Variable Rectangle
+		ctx.clearRect(0+25+(200+50)+100+(75), 0+25+(132/2)+(30/2)+100, 75, 30);
+		ctx.rect(0+25+(200+50)+100+(75), 0+25+(132/2)+(30/2)+100, 75, 30);
+		ctx.stroke();
+		ctx.fillText(letter, 0+25+(200+50)+100+(75)+20, 0+25+(132/2)+(30/2)+100+20);
+		ctx.fillText("Variable", 0+25+(200+50)+100+(75), 0+25+(132/2)+(30/2)+100-4);
+
+		//	Value Rectangle
+		ctx.clearRect(0+25+(200+50)+100+(75+75), 0+25+(132/2)+(30/2)+100, 75, 30);
+		ctx.rect(0+25+(200+50)+100+(75+75), 0+25+(132/2)+(30/2)+100, 75, 30);
+		ctx.stroke();
+		ctx.fillText(const_value, 0+25+(200+50)+100+(75+75)+20, 0+25+(132/2)+(30/2)+100+20);
+		ctx.fillText("Value", 0+25+(200+50)+100+(75+75), 0+25+(132/2)+(30/2)+100-4);
+
+		ctx.fillText("Machine D", 0+25+(200+50)+100+(185/2), 0+25+(132/2)+100+(30)+2*14);
+
+		//	Connect to other machines
+		ctx.moveTo(0+25+(200+50)+100+(75*3), 0+25+(132/2)+(30)+100);
+		ctx.lineTo(0+25+(200+50)+100+(185+50)+100, 0+25+(132/2)+(30)+100);
+		ctx.stroke();
+
+		ctx.moveTo(0+25+(200+50)+100+(75*3), 0+25+(132/2)+(30)+100);
+		ctx.lineTo(0+25+(200+50)+100+(185+50)+100, 0+25+(132/2)+(30)+100+30+100);
+		ctx.stroke();
 	}
 }
 
@@ -139,9 +248,39 @@ class MachineA {
 			var stored = this.machine_d.store(lhs, e_result);
 			if(stored) {
 				console.log(lhs + " = " + rhs + " = " + e_result);
+				this.draw(rhs, e_result);
 				return e_result;
 			}
 		}
+	}
+
+	draw(rhs, e_result) {
+		var machines_canvas = document.getElementById("machines_canvas");
+		var ctx = machines_canvas.getContext("2d");
+
+		//	Draw connection line
+		//	X = origin + initial offset + machine I input box width + machine I result width
+		//	Y = origin + initial offset + machine I input/result box height / 2
+		ctx.moveTo(0+25+200+50, 0+25+132/2);
+		ctx.lineTo(0+25+200+50+100, 0+25+132/2);
+		ctx.stroke();
+
+		//	Right hand side (rhs) rectangle
+		ctx.clearRect(0+25+200+50+100, 0+25+132/2-(30/2), 185, 30);
+		ctx.rect(0+25+200+50+100, 0+25+132/2-(30/2), 185, 30);
+		ctx.stroke();
+		ctx.font = "14px Arial";
+		ctx.fillText(rhs, 0+25+200+50+100+20, 0+25+132/2-(30/2)+20);
+		ctx.fillText("Right Hand Side", 0+25+200+50+100, 0+25+132/2-(30/2)-4);
+		
+		//	Machine E result rectangle
+		ctx.clearRect(0+25+200+50+100+185, 0+25+132/2-(30/2), 50, 30);
+		ctx.rect(0+25+200+50+100+185, 0+25+132/2-(30/2), 50, 30);
+		ctx.stroke();
+		ctx.fillText(e_result, 0+25+200+50+100+185+15, 0+25+132/2-(30/2)+20);
+		ctx.fillText("Result", 0+25+200+50+100+185, 0+25+132/2-(30/2)-4);
+
+		ctx.fillText("Machine A", 0+25+200+50+100+185/2, 0+25+132/2+2*14);
 	}
 }
 
@@ -165,9 +304,47 @@ class MachineE {
 			var machine_t = new MachineT(term, this.machine_d);
 			var product = machine_t.run();
 			sum += product;
+
+			this.draw(term, product, sum);
 		}
 
 		return sum;
+	}
+
+	draw(term, result, sum) {
+		var machines_canvas = document.getElementById("machines_canvas");
+		var ctx = machines_canvas.getContext("2d");
+
+		//	Draw connection line
+		//	X = origin + initial offset + machine I input width + machine I result width + connector + machine A width
+		//	Y = origin + initial offset + machine I input/result box height / 2
+		ctx.moveTo(0+25+200+50+100+185+50, 0+25+132/2);
+		ctx.lineTo(0+25+200+50+100+185+50+100, 0+25+132/2);
+		ctx.stroke();
+
+		//	Term rectangle
+		ctx.clearRect(0+25+200+50+100+185+50+100, 0+25+132/2-(30/2), 100, 30);
+		ctx.rect(0+25+200+50+100+185+50+100, 0+25+132/2-(30/2), 100, 30);
+		ctx.stroke();
+		ctx.font = "14px Arial";
+		ctx.fillText(term, 0+25+200+50+100+185+50+100+20, 0+25+132/2-(30/2)+20);
+		ctx.fillText("Term", 0+25+200+50+100+185+50+100, 0+25+132/2-(30/2)-4);
+
+		//	Result rectangle
+		ctx.clearRect(0+25+200+50+100+185+50+100+100, 0+25+132/2-(30/2), 75, 30);
+		ctx.rect(0+25+200+50+100+185+50+100+100, 0+25+132/2-(30/2), 75, 30);
+		ctx.stroke();
+		ctx.fillText(result, 0+25+200+50+100+185+50+100+100+20, 0+25+132/2-(30/2)+20);
+		ctx.fillText("Result", 0+25+200+50+100+185+50+100+100, 0+25+132/2-(30/2)-4);
+
+		//	Sum rectangle
+		ctx.clearRect(0+25+200+50+100+185+50+100+100+75, 0+25+132/2-(30/2), 50, 30);
+		ctx.rect(0+25+200+50+100+185+50+100+100+75, 0+25+132/2-(30/2), 100, 30);
+		ctx.stroke();
+		ctx.fillText(sum, 0+25+200+50+100+185+50+100+100+75+20, 0+25+132/2-(30/2)+20);
+		ctx.fillText("Sum", 0+25+200+50+100+185+50+100+100+75, 0+25+132/2-(30/2)-4);
+
+		ctx.fillText("Machine E", 0+25+200+50+100+185+50+100+(100+75)/2, 0+25+132/2+2*14);
 	}
 }
 
@@ -188,19 +365,56 @@ class MachineT {
 			if (isConstant(factor)) {
 				console.log("Constant factor: " + factor + "\n");
 				product *= factor;
+				this.draw(factor, factor, product);
 			} else if (isExponentiation(factor)) {
 				console.log("Exponentiation factor: " + factor + "\n");
 				var machine_p = new MachineP(factor, this.machine_d);
 				var p_result = machine_p.run();
 				product *= p_result;
+				this.draw(factor, p_result, product);
 			} else if (isVariable(factor)) {
 				console.log("Variable factor: " + factor + "\n");
 				var d_result = this.machine_d.load(factor);
 				product *= d_result;
+				this.draw(factor, d_result, product);
 			}
 		}
 
 		return product;
+	}
+
+	draw(factor, result, product) {
+		var machines_canvas = document.getElementById("machines_canvas");
+		var ctx = machines_canvas.getContext("2d");
+
+		//	Draw connection line
+		ctx.moveTo(0+25+(200+50)+100+(185+50)+100+(100/2), 0+25+(132/2)+(30/2));
+		ctx.lineTo(0+25+(200+50)+100+(185+50)+100+(100/2), 0+25+(132/2)+(30/2)+100);
+		ctx.stroke();
+
+		//	Factor rectangle
+		ctx.clearRect(0+25+(200+50)+100+(185+50)+100, 0+25+132/2+(30/2)+100, 100, 30);
+		ctx.rect(0+25+(200+50)+100+(185+50)+100, 0+25+132/2+(30/2)+100, 100, 30);
+		ctx.stroke();
+		ctx.font = "14px Arial";
+		ctx.fillText(factor, 0+25+(200+50)+100+(185+50)+100+20, 0+25+132/2+(30/2)+100+20);
+		ctx.fillText("Factor", 0+25+(200+50)+100+(185+50)+100, 0+25+132/2+(30/2)+100-4);
+		
+		//	Result rectangle
+		ctx.clearRect(0+25+(200+50)+100+(185+50)+100+(100), 0+25+132/2+(30/2)+100, 75, 30);
+		ctx.rect(0+25+(200+50)+100+(185+50)+100+(100), 0+25+132/2+(30/2)+100, 75, 30);
+		ctx.stroke();
+		ctx.fillText(result, 0+25+(200+50)+100+(185+50)+100+(100)+20, 0+25+132/2+(30/2)+100+20);
+		ctx.fillText("Result", 0+25+(200+50)+100+(185+50)+100+(100), 0+25+132/2+(30/2)+100-4);
+
+		//	Product rectangle
+		ctx.clearRect(0+25+(200+50)+100+(185+50)+100+(100+75), 0+25+132/2+(30/2)+100, 75, 30);
+		ctx.rect(0+25+(200+50)+100+(185+50)+100+(100+75), 0+25+132/2+(30/2)+100, 100, 30);
+		ctx.stroke();
+		ctx.fillText(product, 0+25+(200+50)+100+(185+50)+100+(100+75)+20, 0+25+132/2+(30/2)+100+20);
+		ctx.fillText("Product", 0+25+(200+50)+100+(185+50)+100+(100+75), 0+25+132/2+(30/2)+100-4);
+
+		ctx.fillText("Machine T", 0+25+(200+50)+100+(185+50)+100+(100+75)/2, 0+25+132/2+(30/2)+100+3*14);
 	}
 }
 
@@ -230,7 +444,36 @@ class MachineP {
 
 		// Use the result value to compute the exponential value and return it to the sender.
 		//console.log("The result of the exponential is: " + Math.pow(d_result, constant_term));
-		return Math.pow(d_result, constant_term);
+		var pow_result = Math.pow(d_result, constant_term);
+		this.draw(this.expression, pow_result);
+		return pow_result;
+	}
+
+	draw(pow_expr, result) {
+		var machines_canvas = document.getElementById("machines_canvas");
+		var ctx = machines_canvas.getContext("2d");
+
+		//	Draw connection line
+		ctx.moveTo(0+25+(200+50)+100+(185+50)+100+(100/2), 0+25+(132/2)+(30/2)+100+(30));
+		ctx.lineTo(0+25+(200+50)+100+(185+50)+100+(100/2), 0+25+(132/2)+(30/2)+100+(30)+100);
+		ctx.stroke();
+		
+		//	Power Expression rectangle
+		ctx.clearRect(0+25+(200+50)+100+(185+50)+100, 0+25+132/2+(30/2)+100+(30)+100, 100, 30);
+		ctx.rect(0+25+(200+50)+100+(185+50)+100, 0+25+132/2+(30/2)+100+(30)+100, 100, 30);
+		ctx.stroke();
+		ctx.font = "14px Arial";
+		ctx.fillText(pow_expr, 0+25+(200+50)+100+(185+50)+100+20, 0+25+132/2+(30/2)+100+(30)+100+20);
+		ctx.fillText("Power", 0+25+(200+50)+100+(185+50)+100, 0+25+132/2+(30/2)+100+(30)+100-4);
+		
+		//	Result rectangle
+		ctx.clearRect(0+25+(200+50)+100+(185+50)+100+(100), 0+25+132/2+(30/2)+100+(30)+100, 100, 30);
+		ctx.rect(0+25+(200+50)+100+(185+50)+100+(100), 0+25+132/2+(30/2)+100+(30)+100, 100, 30);
+		ctx.stroke();
+		ctx.fillText(result, 0+25+(200+50)+100+(185+50)+100+(100)+20, 0+25+132/2+(30/2)+100+(30)+100+20);
+		ctx.fillText("Result", 0+25+(200+50)+100+(185+50)+100+(100), 0+25+132/2+(30/2)+100+(30)+100-4);
+
+		ctx.fillText("Machine P", 0+25+(200+50)+100+(185+50)+100+(100+100)/3, 0+25+132/2+(30/2)+100+(30)+100+3*14);
 	}
 }
 
